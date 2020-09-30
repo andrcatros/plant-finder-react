0bb42073957cd593078768f450b69c67777562b9
NANOUU <59022292+antoniasymeonidou@users.noreply.github.com>
2020-09-30 21:58:48 +0100

    Add files via upload

--- register.js
+++ register.js
@@ -1,73 +1,36 @@
-import React, { Component } from ‘react’;
-import { Link } from ‘react - router - dom’;
-class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fullname: ‘’,
-            email: ‘’,
-            password: ‘’
-        };
-        this.update = this.update.bind(this);
-        this.displayLogin = this.displayLogin.bind(this);
-    }
-    update(e) {
-        let name = e.target.name;
-        let value = e.target.value;
-        this.setState({
-            [name]: value
-        });
-    }
-    displayLogin(e) {
-        e.preventDefault();
-        console.log(‘You have successfully registered’);
-        console.log(this.state);
-        this.setState({
-            fullname: ‘’,
-            email: ‘’,
-            password: ‘’
-        });
-    }
-    render() {
-        return (
-			<div className=“register”>
-				<form onSubmit={this.displayLogin}>
-					<h2>Register</h2>
-					<div className=“name”>
-						<input
-							type=“text”
-							placeholder=“Full Name”
-							name=“fullname”
-							value={this.state.fullname}
-							onChange={this.update}
-						/>
-					</div>
-					<div className=“email”>
-						<input
-							type=“text”
-							placeholder=“Enter your email”
-							name=“email”
-							value={this.state.email}
-							onChange={this.update}
-						/>
-					</div>
-					<div className=“pasword”>
-						<input
-							type=“password”
-							placeholder=“Password”
-							name=“password”
-							value={this.state.password}
-							onChange={this.update}
-						/>
-					</div>
-					<div className=“password”>
-						<input type=“password” placeholder=“Confirm Password” name=“password1” />
-					</div>
-					<input type=“submit” value=“Login” />
-				</form >
-            <Link to=“ /”> Login Here</Link >
-			</div >
-		);
-    }
+import React from 'react';
+import loginImg from '../styles/open-doodles-plant.svg';
+
+export class Register extends React.Component {
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Register</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} alt=""/>
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="text" name="password" placeholder="password" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Register
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-export default Register;

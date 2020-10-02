814c471ed9dd8c6131b57cd72723db2b6b1b9d3e
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-10-02 14:15:38 +0100

    simple Profile

---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Profile = ({
+  firstName,
+  lastName,
+  imageURL
+}) => (
+  <div>
+    <div>
+      <img src={ imageURL } className='avatar-image' />
+    </div>
+    Name: { firstName } { lastName }
+  </div>
+)
+
+export default Profile
